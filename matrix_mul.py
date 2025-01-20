e2aef0677eec96390ac009e4674cbb7e42ba32b0
NiyonshutiDavid <144002340+NiyonshutiDavid@users.noreply.github.com>
2025-01-20 22:54:03 +0200

    Update matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
 
-# Load data
-data = pd.read_csv('path/to/data')
-
 # Example arrays
 Prices = [[300, 500],
           [1000, 120.85]]
 
 Array2 = [200, 100]
 
-# Calculate the result
+# Initialization of empty list Ans to store the result
 Ans = []
 # (300*200 + 500*100) as an example calculation
 
@@ -27,3 +24,4 @@ for i in range(len(Prices)):
 
 
 print(Ans)
+#Output for the product will be [110000.0, 212085.0]
